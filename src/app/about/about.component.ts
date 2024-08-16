7e7f3d8b19d1e2289f75d45a710eb04119b9a2af
Suvam_Chanda <ssuvam08@gmail.com>
2024-08-16 23:38:33 +0530

    Add Form

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker'; // If using Angular Material
+import { MatNativeDateModule } from '@angular/material/core'; // Required for Angular Material Datepicker
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatDatepickerModule, MatNativeDateModule,CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  myForm!: FormGroup;
 
+  dropdownOptions = ['Option 1', 'Option 2', 'Option 3'];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      dropdown: ['', Validators.required],
+      textbox: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]], // Set exact length to 9
+      datepicker: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
